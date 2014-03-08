9bea5ffbbdc4e3f4c9f6c0a8284452b0079a909f
Kai Yao <yky234-github@yahoo.com.sg>
2014-03-08 17:05:45 +0800

    Make account.php edit account if user is logged in

--- account.php
+++ account.php
@@ -3,17 +3,56 @@
 
   echo "test1";
 
-  if (isset($_POST['username'])){
-    echo "test2";
+  if (isset($_POST['edit-mode'])){
+    if ($_POST['username'] == $_SESSION['username']){
+      $stmt = null;
+      echo 1;
+      if (trim($_POST['password']) != '' && $_POST['password'] == $_POST['retype-password']){
+        echo 2;
 
-    if ($_POST['password'] != $_POST['retype-password']){
-      echo "test3";
-      $err = 'Passwords do not match';
-    }else{
-      echo "test4";
-      if ($stmt = $conn->prepare("INSERT INTO user (email, username, password, phone) VALUES (?, ?, ?, ?)")) {
-        $stmt->bind_param('ssss', $_POST['email'], $_POST['username'], $_POST['password'], $_POST['contact-number']);
+        $stmt = $conn->prepare("UPDATE user SET email = ?, password = ?, phone = ? WHERE username = ?");
+        $stmt->bind_param('ssss', $_POST['email'], $_POST['password'], $_POST['contact-number'], $_SESSION['username']);
         $stmt->execute();
+        if ($conn->error) echo $conn->error;
+      }else{
+        $stmt = $conn->prepare("UPDATE user SET email = ?,  phone = ? WHERE username = ?");
+        $stmt->bind_param('sss', $_POST['email'], $_POST['contact-number'], $_SESSION['username']);
+        $stmt->execute();
+        if ($conn->error) echo $conn->error;
+      }
+    }
+  }else{
+
+    if (isset($_POST['username'])){
+      echo "test2";
+
+      if ($_POST['password'] != $_POST['retype-password']){
+        echo "test3";
+        $err = 'Passwords do not match';
+      }else{
+        echo "test4";
+        if ($stmt = $conn->prepare("INSERT INTO user (email, username, password, phone) VALUES (?, ?, ?, ?)")) {
+          $stmt->bind_param('ssss', $_POST['email'], $_POST['username'], $_POST['password'], $_POST['contact-number']);
+          $stmt->execute();
+        }
+      }
+    }
+
+  }
+
+  $editMode = false;
+
+  if (isset($_SESSION['username'])){
+    echo "{$_SESSION['username']} is logged in";
+
+    if ($stmt = $conn->prepare("SELECT * FROM user WHERE username = ?")) {
+      $stmt->bind_param('s', $_SESSION['username']);
+      $stmt->execute();
+      $res = $stmt->get_result();
+      if ($res->num_rows >= 1) {
+        $editMode = true;
+
+        $row = $res->fetch_assoc();
       }
     }
   }
@@ -57,13 +96,30 @@
       ?>
 
       <form class="form-signin" role="form" action="account.php" method="post">
-        <h2 class="form-signin-heading">Create account</h2>
-        <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
-        <input type="password" name="password" class="form-control" placeholder="Password" required>
-		<input type="password" name="retype-password" class="form-control" placeholder="Retype Password" required>
-		<input type="email" name="email" class="form-control" placeholder="Email Address" required>
-		<input type="text" name="contact-number" class="form-control" placeholder="Contact Number" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Create account</button>
+        <h2 class="form-signin-heading">
+          <?php
+            if ($editMode) {
+              echo 'Edit account';
+              echo '<input type="hidden" name="edit-mode">';
+            }else{
+              echo 'Create account';
+            }
+          ?>
+        </h2>
+        <input type="text" name="username" class="form-control form-first-item" placeholder="Username" <?= $editMode? 'readonly' : '' ?> value="<?= $editMode ? $row['username'] : ''; ?>" required autofocus>
+        <input type="password" name="password" class="form-control" placeholder="Password" <?= $editMode ? '' : 'required' ?>>
+        <input type="password" name="retype-password" class="form-control" placeholder="Retype Password" <?= $editMode ? '' : 'required' ?>>
+        <input type="email" name="email" class="form-control" placeholder="Email Address" value="<?= $editMode ? $row['email'] : ''; ?>" required>
+        <input type="text" name="contact-number" class="form-control form-last-item" placeholder="Contact Number" value="<?= $editMode ? $row['phone'] : ''; ?>" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">
+          <?php
+            if ($editMode) {
+              echo 'Edit account';
+            }else{
+              echo 'Create account';
+            }            
+          ?>
+        </button>
       </form>
 
     </div> <!-- /container -->
