cb366709f3e1a9834c0f9ad80025339a3f6625d3
Kai Yao <yky234-github@yahoo.com.sg>
2014-03-16 22:23:55 +0800

    Playing around with form on view_all_items

--- view_all_items.php
+++ view_all_items.php
@@ -46,8 +46,8 @@
 $extra_head = <<<EOT
   <style>
   .row{
-    margin-top:20px;
-    margin-bottom:20px;
+	margin-top:20px;
+	margin-bottom:20px;
   }
   </style>
 EOT;
@@ -60,154 +60,153 @@ include('header.php');
 	<h1>All listed items</h1>
 					
 		<?php
-        if (isset($error) && $error == "NoItemPosted"){
+		if (isset($error) && $error == "NoItemPosted"){
 		?>
 			<p>No items posted yet!</p>
-            <p>Click <a href="add_modify_item.php">here</a> to start.</p>
+			<p>Click <a href="add_modify_item.php">here</a> to start.</p>
 		<?php
-        }else if(isset($error) && $error == "ErrorSQL"){
+		}else if(isset($error) && $error == "ErrorSQL"){
 		?>
 			<p>Error URL</p>
 			<p>Click <a href="view_all_items.php">here</a> to view all items!</p>
 	<?php
-	    }else{
-     ?>
-        <div class="row">
-            <form name="form" method="GET" action="view_all_items.php">
-                <div class="col-md-6">
-                    <b>Sort By:</b>						
-                    <!--ORDER BY-->
-                    <select name="orderby" style="width:200px;display:inline-block;" class="form-control">
-                    <?php 
+		}else{
+	 ?>
+		<div class="well">
+			<form name="form" method="GET" action="view_all_items.php" class="form-inline">
+				<div class="form-group">
+					<label>Sort By</label>					
+					<!--ORDER BY-->
+					<select name="orderby" style="width:200px;display:inline-block;" class="form-control">
+					<?php 
 
-                        $orderby='';;
-                        $sel="selected";
+						$orderby='';;
+						$sel="selected";
 
-                        if(isset($_GET['orderby'])){ 
-                            $orderby=$_GET['orderby'];
-                        } ?>
+						if(isset($_GET['orderby'])){ 
+							$orderby=$_GET['orderby'];
+						} ?>
 
-                        <option value="date_listed" <?php if (($orderby === 'date_listed') OR ($orderby === '')) echo $sel; ?>>Date Listed</option>
-                        <option value="title" <?php if ($orderby === 'title') echo $sel; ?>>Title</option>
-                        <option value="price" <?php if ($orderby === 'price') echo $sel; ?>>Price</option>
-                        <option value="cond" <?php if ($orderby === 'cond') echo $sel; ?>>Condition</option>
-                    </select>
+						<option value="date_listed" <?php if (($orderby === 'date_listed') OR ($orderby === '')) echo $sel; ?>>Date Listed</option>
+						<option value="title" <?php if ($orderby === 'title') echo $sel; ?>>Title</option>
+						<option value="price" <?php if ($orderby === 'price') echo $sel; ?>>Price</option>
+						<option value="cond" <?php if ($orderby === 'cond') echo $sel; ?>>Condition</option>
+					</select>
 
-                    <!--ORDER -->
-                    <select name="order" style="width:200px;display:inline-block;" class="form-control">
-                    <?php 
-                        $order=$_GET['order'];
-                        $sel="selected";
-                        if(isset($_GET['order'])){ 
-                            $order=$_GET['order'];
-                            $sel="selected";
-                        } ?>
-                        <option value="DESC" <?php if (($order === 'DESC') OR ($order === '')) echo $sel; ?>>Descending</option>
-                        <option value="ASC" <?php if ($order === 'ASC') echo $sel; ?>>Ascending</option>						
-                    </select>
-                </div>
-                <div class="col-md-4">
-                    <b>Filter Category:</b>
-                    <!--FILTER BY CATEGORY-->
-                    <select name="cat" style="width:200px;display:inline-block;"  class="form-control">
-                        <?php 
-                        $catname="";
-                        $sel="selected";
+					<!--ORDER -->
+					<select name="order" style="width:200px;display:inline-block;" class="form-control">
+					<?php 
+						$order=$_GET['order'];
+						$sel="selected";
+						if(isset($_GET['order'])){ 
+							$order=$_GET['order'];
+							$sel="selected";
+						} ?>
+						<option value="DESC" <?php if (($order === 'DESC') OR ($order === '')) echo $sel; ?>>Descending</option>
+						<option value="ASC" <?php if ($order === 'ASC') echo $sel; ?>>Ascending</option>						
+					</select>
+				</div>
+				<div class="form-group">
+					<label>Filter Category</label>	
+					<!--FILTER BY CATEGORY-->
+					<select name="cat" style="width:200px;display:inline-block;"  class="form-control">
+						<?php 
+						$catname="";
+						$sel="selected";
 
-                        if(isset($_GET['cat'])){
-                            $catname=$_GET['cat'];
-                        } ?>
-                            <?php
+						if(isset($_GET['cat'])){
+							$catname=$_GET['cat'];
+						} ?>
+							<?php
 
-                        if ($allCatStmt = $conn->prepare("SELECT * FROM category")) {
+						if ($allCatStmt = $conn->prepare("SELECT * FROM category")) {
 
-                            $allCatStmt->execute();
-                            $allCatRes = $allCatStmt->get_result();
-                                ?>
-                            <option value="" <?php if($catname == "") echo $sel ?>> -- View All --</option>			
-                          <?php while($cat=$allCatRes->fetch_assoc()){
+							$allCatStmt->execute();
+							$allCatRes = $allCatStmt->get_result();
+								?>
+							<option value="" <?php if($catname == "") echo $sel ?>> -- View All --</option>			
+						  <?php while($cat=$allCatRes->fetch_assoc()){
 
-                                ?>
-                                <option value="<?php echo $cat['name']?>" 
-                                    <?php if($cat['name'] == $catname) echo $sel?>>
-                                    <?php echo $cat['name']?></option>
-                            <?php 
-                              }
+								?>
+								<option value="<?php echo $cat['name']?>" 
+									<?php if($cat['name'] == $catname) echo $sel?>>
+									<?php echo $cat['name']?></option>
+							<?php 
+							  }
 
-                          }	?>
+						  }	?>
 
-                    </select>
-                </div>
-                <div class="col-md-2">
+					</select>
+				</div>
+				<div class="form-group">
 						
-                    <button class="btn btn-primary" type="submit">Go</button>
-                    <a href="?" class="btn btn-primary">Reset</a>
-                </div>
-            </form>
-        </div>
-
-        <?php	    
-            while($item=$res ->fetch_assoc()){		
+					<button class="btn btn-primary" type="submit">Go</button>
+					<a href="?" class="btn btn-primary">Reset</a>
+				</div>
+			</form>
+		</div>
+		<?php	    
+			while($item=$res ->fetch_assoc()){		
 		?>
-        <div class="row">
-        <div class="col-md-3">
-        <?php 
+		<div class="row">
+		<div class="col-md-3">
+		<?php 
 			$itemphoto='img/noimg.jpg';
 			if($item['photo']!= NULL) 
 				$itemphoto=$item['photo']; ?>
 			
 			<!--PUT IMAGE HERE-HIDDEN UNLESS NOT NULL-->
 			<a href="view_item.php?id=<?php echo $item['id'] ?>" title="Click to find out more!"><img src="<?php echo $itemphoto ?>" class="img-thumbnail" style="max-width:200px"></a>
-        </div>
-        <div class="col-md-9">
-            <h2><a href="view_item.php?id=<?php echo $item['id'] ?>" title="Click to find out more!"><?php echo $item['title'] ?></a></h2>
-            <p>
+		</div>
+		<div class="col-md-9">
+			<h2><a href="view_item.php?id=<?php echo $item['id'] ?>" title="Click to find out more!"><?php echo $item['title'] ?></a></h2>
+			<p>
 				<ul>
-				    <li>
-                        <b>Date Listed:</b>
-                        <?php echo $item['date_listed'] ?>
-                    </li>
-				    <li>
-                        <b>Posted by:</b>
-				        <a href="view_profile.php?username=<?php echo $item['user'] ?>"><?php echo $item['user'] ?></a></li>
-				    <li>
-                        <b>Summary:</b>
-                        <?php echo $item['summary'] ?>
-                    </li>
-				    <li>
-                        <b>Price:</b>
-                        <?php echo $item['price'] ?>
-                    </li>
-                    <li>
-                        <b>Condition:</b>
-                        <?php echo $item['cond'] ?>
-                    </li>
-                    <li>
-                        <b>Categories:</b> 
-                        <?php 
-                            if ($itemCatStmt = $conn->prepare("SELECT cat_name FROM tagged WHERE item_id = ?")) {
-                                $itemCatStmt->bind_param('i', $item['id']);
-                                $itemCatStmt->execute();
-                                $itemCatRes = $itemCatStmt->get_result();
-                                $itemCatAll= array();
-                                while($cat=$itemCatRes ->fetch_assoc()){
-                                    $itemCatAll[] = '<a href="view_all_items.php?cat='.$cat['cat_name'].'">'.$cat['cat_name'].'</a>';  
-                                }
-                            }
-                            echo (implode (", " , $itemCatAll ));?>
-                    </li>		
+					<li>
+						<b>Date Listed:</b>
+						<?php echo $item['date_listed'] ?>
+					</li>
+					<li>
+						<b>Posted by:</b>
+						<a href="view_profile.php?username=<?php echo $item['user'] ?>"><?php echo $item['user'] ?></a></li>
+					<li>
+						<b>Summary:</b>
+						<?php echo $item['summary'] ?>
+					</li>
+					<li>
+						<b>Price:</b>
+						<?php echo $item['price'] ?>
+					</li>
+					<li>
+						<b>Condition:</b>
+						<?php echo $item['cond'] ?>
+					</li>
+					<li>
+						<b>Categories:</b> 
+						<?php 
+							if ($itemCatStmt = $conn->prepare("SELECT cat_name FROM tagged WHERE item_id = ?")) {
+								$itemCatStmt->bind_param('i', $item['id']);
+								$itemCatStmt->execute();
+								$itemCatRes = $itemCatStmt->get_result();
+								$itemCatAll= array();
+								while($cat=$itemCatRes ->fetch_assoc()){
+									$itemCatAll[] = '<a href="view_all_items.php?cat='.$cat['cat_name'].'">'.$cat['cat_name'].'</a>';  
+								}
+							}
+							echo (implode (", " , $itemCatAll ));?>
+					</li>		
 				</ul>
-            </div>
-        </div>
-        <?php
+			</div>
+		</div>
+		<?php
 	   }	
-        ?>
+		?>
 
 	
 	  
-    <?php
-    }	
-    ?>
+	<?php
+	}	
+	?>
 	 
 <?php
   include('footer.php');
